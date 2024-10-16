dc412a67ab97d58fc60a883df735557c9d0add93
Aryan <chaudharyaryan3456@gmail.com>
2024-10-16 23:06:01 +0530

    Added some methods

--- loops.js
+++ loops.js
@@ -1,28 +1,28 @@
-// // const n = 3;
+// const n = 3;
 
-// // for(let i = 1; i <= 10; i++){
-// //     console.log(`${n} * ${i} = ${n * i}`);
-// // }
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${n} * ${i} = ${n * i}`);
+// }
 
-// // const student = {
-// //     name : "Monica",
-// //     class : 7,
-// //     age : 12
-// // }
+// const student = {
+//     name : "Monica",
+//     class : 7,
+//     age : 12
+// }
 
 // // for(let key in student){
 // //     console.log(`${key} => ${student[key]}`);
 // // }
 
-// // Print a pattern like below :
-// // 1 
-// // 1 2
-// // 1 2 3
-// // 1 2 3 4
-// // 1 2 3 4 5 
-// // 1 2 3 4 5 6 
-// // 1 2 3 4 5 6 7
-// // 1 2 3 4 5 6 7 8
+// Print a pattern like below :
+// 1 
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5 
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7 8
 
 // for(let i = 1; i <= 8; i++){
 //     let pattern = "";
@@ -34,7 +34,7 @@
 
 
 
-// // Given an array, find out the total no. of odd and even nos.?
+// Given an array, find out the total no. of odd and even nos.?
 // let arrs = [1, 2, 5, 7, 8, 3, 9, 1, 3, 4, 5];
 // let Even_No = 0;
 // let Odd_No = 0;
@@ -54,9 +54,9 @@
 //     text = "The Number is " + i;
 //     i++;
 //     console.log(text);
-// // }
+// }
 
-// // Do-while loops 
+// Do-while loops 
 // let i = 1;
 // do{
 //     console.log("Hello Loop");
@@ -80,13 +80,13 @@
 // console.log(string3);
 
 
-// // Concat() Method of String
-// // var str1 = "Hello";
-// // var str2 = "Maria!";
-// // var res = str1.concat(str2);
-// // console.log(res);
+// Concat() Method of String
+// var str1 = "Hello";
+// var str2 = "Maria!";
+// var res = str1.concat(str2);
+// console.log(res);
 
-// // Slice Method of String 
+// Slice Method of String 
 // var name = "You will be an amazing developer";
 // var newMessage = name.slice(7, 13);
 // console.log(newMessage);
@@ -110,8 +110,44 @@
 // console.log(name.replaceAll(' ', '_'));
 // console.log(name.trim());
 
-// // Split method of String
+// Split method of String
 // console.log(name.split("will"));
-
-// // length method of String
+// length method of String
 // console.log(name.length);
+// Question - You are provided with a string you have to slice the name from the string and print the sliced string. 
+// Input = Hi prepbytes
+// Output = prepbytes
+// var word = "Hi Prepbytes";
+// console.log(word.slice(2));
+// Array 
+//(i). Push() Method - The push() mehtod adds new elemnts to the end of an array, and returns the new length.
+// const arr = ["Aryan", "The"];
+// arr.push("Developer");
+// console.log(arr);
+// Output = ["Aryan", "The", "Developer"];
+// (ii). slice() method - The slice() method selects a  part of an array and returns the new array. 
+// const arr = ["Aryan", "The", "Developer"];
+// console.log(arr.slice(1, 2));
+// Output = ["The"];
+// (iii). toString() Method - The toString() method converts an array to a String, and returns the result. 
+// const arr = ["Jarvis", "The", "Developer"];
+// console.log(arr.toString());
+// Output = "Jarvis, The, Developer"
+// (iv). shift() Method  - The shift() method removes the first element of an Array, and returns that element. 
+// const arr = ["Aryan", "The", "Developer"];
+// arr.shift();
+// console.log(arr);
+// Output - ["The", "Developer"];
+// (v). map() method - The map() method creates a new array with the results of calling a function for every array element.
+// const arr = [1, 4, 9, 16 ];
+// console.log(arr.map(x => x * 2));
+// Output = [2, 8, 18, 32];
+// (vi). pop() Method - The pop() method removes the last element of an Array, and returns that element. 
+// const arr = ["Aryan", "The", "Developer"];
+// arr.pop();
+// console.log(arr);
+// Output = ["Aryan", "The"]
+// (vii). filter() method - The filter() method creates an array filled with all array elements that pass a test (provided as a function). 
+// const arr = ["Aryan", "The", "Developer"];
+// console.log(arr.filter(word => word.length > 3));
+// Output = ["Aryan", "The"];
