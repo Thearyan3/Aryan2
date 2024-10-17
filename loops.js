032dedce9ed2b3a2bc72ca807424d924f3695aff
Aryan <chaudharyaryan3456@gmail.com>
2024-10-17 13:25:19 +0530

    added some more methods

--- loops.js
+++ loops.js
@@ -116,6 +116,12 @@
 // length method of String
 // console.log(name.length);
 
+// Reverse Method of String
+// let array = [1, 2, 3, 4, 5];
+// array.reverse();
+// console.log(array);
+// Output = [5, 4, 3, 2, 1];
+
 // Question - You are provided with a string you have to slice the name from the string and print the sliced string. 
 // Input = Hi prepbytes
 // Output = prepbytes
@@ -161,4 +167,43 @@
 // const arr = ["Aryan", "The", "Developer"];
 // console.log(arr.filter(word => word.length > 3));
 // Output = ["Aryan", "The"];
+
+// (viii). includes() Method
+// let array = [1, 2, 3, 4, 5];
+// array.includes(5);
+// console.log(array);
+
+// (ix). unshift() Method
+// // let array = [1, 2, 3, 4, 5];
+// // array.unshift(10);
+// // console.log(array);
+
+// (x). concat() and join() Metod 
+// let myArr = [1, 2, 3];
+// console.log(myArr.concat(5, 6, 7));
+// let MyArr = ["Hi", "How", "Are", "You"];
+// console.log(MyArr.join("-"));
+
+// (xi). indexOf() and lastIndexOf() Method 
+// let myArr = [1, 2, 3, 4, 5, 6, 3];
+// console.log(myArr.indexOf(3));
+// console.log(myArr.lastIndexOf(3));
+
+// (xii). slice() and splice() method 
+// let myArr = [1, 2, 3, 4, 5, 6, 3];
+// console.log(myArr.slice(1, 3));
+// console.log(myArr.splice(1, 3, "a","b","c" ,"d"));
+
+// (xiii).  reverse() and sort() method
+// let myArr = [1, 2, 3, 4, 5, 6, 3];
+// console.log(myArr.reverse());
+// console.log(myArr.sort());
+
+// // Question - You are provided an array of integer and you have to print the sum of array. 
+// let arr = [1, 2, 3, 4, 66, 23, 28, 448];
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//         sum = sum + arr[i];
+// }
+// console.log(sum);
 
